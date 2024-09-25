53587f9c3f6915b082e00c7485103cce2db2d2fa
SangeethRaj3 <105667525+SangeethRaj3@users.noreply.github.com>
2024-09-25 19:50:11 +0530

    Update SSS.js

--- SSS.js
+++ SSS.js
@@ -1,1 +1,19 @@
 window.ContentOfTheMail = "Hello Universe From THe Earth";
+
+// URL of the API endpoint
+const url = 'https://app.apptivo.com/app/dao/v6/customers?a=getConfigData&_='+Number(new Date())+'&objectId=3';
+
+// Making a GET request
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json(); // Parsing the JSON response
+  })
+  .then(data => {
+    console.log(data); // Handling the data
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
